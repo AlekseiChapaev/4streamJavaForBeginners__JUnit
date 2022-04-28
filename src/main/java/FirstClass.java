05443913b40c398456c7c388b5785c9e9ea24cf6
Aleksei Chapaev <alekseiachapaev@gmail.com>
2022-04-28 23:31:01 +0200

    refactoring code

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -2,7 +2,7 @@ public class FirstClass {
 
     public int[] ascendingSequence(int start, int end, int step) {
         if (step >= 0 && start < end) {
-            int[] result = new int[end - start + 1];
+            int[] result = new int[(end - start + 1) / step];
             int j = 0;
             for (int i = start; i <= end; i += step) {
                 if (j < result.length) {
