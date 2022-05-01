c3a4a124c53a88eae9591c93ee71e5384d74caa2
Aleksei Chapaev <alekseiachapaev@gmail.com>
2022-05-01 22:42:09 +0200

    add hw_9 task3

---
+++ src/main/java/hw_9/Task_3_SumArray.java
@@ -0,0 +1,15 @@
+package hw_9;
+
+public class Task_3_SumArray {
+
+    public int getSumArray(int[] arr) {
+        if (arr != null && arr.length > 0) {
+            int sum = 0;
+            for (int i = 0; i < arr.length; i++) {
+                sum += arr[i];
+            }
+            return sum;
+        }
+        return 0;
+    }
+}
---
+++ src/test/java/hw_9/Task_3_SumArrayTest.java
@@ -0,0 +1,32 @@
+package hw_9;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class Task_3_SumArrayTest {
+
+    @Test
+    public void testSumArrayHappyPathTestData1(){
+        Task_3_SumArray sumArray = new Task_3_SumArray();
+        Assertions.assertEquals(15, sumArray.getSumArray(new int[]{0, 1, 2, 3, 4, 5}));
+    }
+
+    @Test
+    public void testSumArrayHappyPathTestData2(){
+        Task_3_SumArray sumArray = new Task_3_SumArray();
+        Assertions.assertEquals(-10, sumArray.getSumArray(new int[]{-7, -3}));
+    }
+
+    @Test
+    public void testSumArrayHappyPathEmptyArray(){
+        Task_3_SumArray sumArray = new Task_3_SumArray();
+        Assertions.assertEquals(0, sumArray.getSumArray(new int[]{}));
+    }
+
+    @Test
+    public void testSumArrayHappyPathNullArray(){
+        Task_3_SumArray sumArray = new Task_3_SumArray();
+        Assertions.assertEquals(0, sumArray.getSumArray(null));
+    }
+
+}
