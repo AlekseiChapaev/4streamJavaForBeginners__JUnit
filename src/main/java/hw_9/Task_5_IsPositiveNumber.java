395035ecad09a17cc5a297bc8d61946b7f1cbac0
Aleksei Chapaev <alekseiachapaev@gmail.com>
2022-05-01 23:05:50 +0200

    add hw_9 task5

---
+++ src/main/java/hw_9/Task_5_IsPositiveNumber.java
@@ -0,0 +1,8 @@
+package hw_9;
+
+public class Task_5_IsPositiveNumber {
+
+    public boolean isPositiveNumber(int num){
+        return num >= 0;
+    }
+}
---
+++ src/test/java/hw_9/IsPositiveNumberTest.java
@@ -0,0 +1,25 @@
+package hw_9;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class IsPositiveNumberTest {
+
+    @Test
+    public void testIsPositiveTestData1(){
+        Task_5_IsPositiveNumber posNum = new Task_5_IsPositiveNumber();
+        Assertions.assertTrue(posNum.isPositiveNumber(555));
+    }
+
+    @Test
+    public void testIsPositiveTestData2(){
+        Task_5_IsPositiveNumber posNum = new Task_5_IsPositiveNumber();
+        Assertions.assertTrue(posNum.isPositiveNumber(0));
+    }
+
+    @Test
+    public void testIsPositiveTestData3(){
+        Task_5_IsPositiveNumber posNum = new Task_5_IsPositiveNumber();
+        Assertions.assertFalse(posNum.isPositiveNumber(-555));
+    }
+}
