8b7c0aaf19d7066c47bd23fd0fc103f7c521ed56
Aleksei Chapaev <alekseiachapaev@gmail.com>
2022-05-09 13:40:45 +0200

    refactoring Task_9_IntersectionTest my second solution

--- src/main/java/hw_9/Task_9_Intersection.java
+++ src/main/java/hw_9/Task_9_Intersection.java
@@ -2,26 +2,70 @@ package hw_9;
 
 public class Task_9_Intersection {
 
-    public int[] getUniqueNumbersArray(int[] arr) {
+    public static int[] getNotUniqueNumbersArray(int[] arr) {
         if (arr != null && arr.length > 0) {
-            int countOfNotUniqueNumbersInArray = 0;
+            int[] tempArray = new int[arr.length];
             for (int i = 0; i < arr.length; i++) {
-                for (int j = i + 1; j < arr.length; j++) {
-                    if (arr[i] == arr[j] && arr[i] != Integer.MIN_VALUE) {
+                tempArray[i] = arr[i];
+            }
+
+            int countOfNotUniqueNumbersInArray = 0;
+            for (int i = 0; i < tempArray.length; i++) {
+                for (int j = i + 1; j < tempArray.length; j++) {
+                    if(tempArray[i] == tempArray[j] && tempArray[j] != Integer.MIN_VALUE){
                         countOfNotUniqueNumbersInArray++;
+                        tempArray[j] = Integer.MIN_VALUE;
+                    }
+                }
+            }
+            int[] notUniqueArr = new int[countOfNotUniqueNumbersInArray];
+            int index = 0;
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = i + 1; j < arr.length; j++){
+                    if(arr[i] == arr[j]){
+                        notUniqueArr[index++] = arr[i];
+                    }
+                }
+            }
+
+            return notUniqueArr;
+        }
+
+        return null;
+        }
+
+    public static int[] getUniqueNumbersArray(int[] arr) {
+        if (arr != null && arr.length > 0) {
+            int[] tempArray = new int[arr.length];
+            for (int i = 0; i < arr.length; i++) {
+                tempArray[i] = arr[i];
+            }
+
+            int countOfNotUniqueNumbersInArray = 0;
+            for (int i = 0; i < tempArray.length; i++) {
+                for (int j = i + 1; j < tempArray.length; j++) {
+                    if(tempArray[i] == tempArray[j] && tempArray[j] != Integer.MIN_VALUE){
+                        countOfNotUniqueNumbersInArray++;
+                        tempArray[j] = Integer.MIN_VALUE;
+                    }
+                }
+            }
+            int[] uniqueArr = new int[arr.length - countOfNotUniqueNumbersInArray];
+            int index = 0;
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = i + 1; j < arr.length; j++){
+                    if(arr[i] == arr[j]){
                         arr[j] = Integer.MIN_VALUE;
                     }
                 }
             }
-            int[] resultArray = new int[arr.length - countOfNotUniqueNumbersInArray];
-            int index = 0;
             for (int i = 0; i < arr.length; i++) {
                 if(arr[i] != Integer.MIN_VALUE){
-                    resultArray[index++] = arr[i];
+                    uniqueArr[index++] = arr[i];
                 }
             }
 
-            return resultArray;
+            return uniqueArr;
         }
 
         return null;
@@ -32,8 +76,7 @@ public class Task_9_Intersection {
         int[] arr1UniqueNumbers = getUniqueNumbersArray(arr1);
         int[] arr2UniqueNumbers = getUniqueNumbersArray(arr2);
 
-        int tempArrlenght = arr1UniqueNumbers.length + arr2UniqueNumbers.length;
-        int[] tempArr = new int[tempArrlenght];
+        int[] tempArr = new int[arr1UniqueNumbers.length + arr2UniqueNumbers.length];
         for (int i = 0; i < arr1UniqueNumbers.length; i++) {
             tempArr[i] = arr1UniqueNumbers[i];
         }
@@ -41,25 +84,8 @@ public class Task_9_Intersection {
             tempArr[arr1UniqueNumbers.length + i] = arr2UniqueNumbers[i];
         }
 
-        int count = 0;
-        for (int i = 0; i < tempArr.length; i++) {
-            for (int j = i + 1; j < tempArr.length; j++){
-                if(tempArr[i] == tempArr[j]){
-                    count++;
-                }
-            }
-        }
-        int[] res = new int[count];
-        int index = 0;
-        for (int i = 0; i < tempArr.length; i++) {
-            for (int j = i + 1; j < tempArr.length; j++){
-                if(tempArr[i] == tempArr[j] && tempArr[j] != Integer.MIN_VALUE){
-                    res[index++] = tempArr[i];
-                    tempArr[j] = Integer.MIN_VALUE;
-                }
-            }
-        }
+        int[] result = getNotUniqueNumbersArray(tempArr);
 
-        return res;
+        return result;
     }
 }
