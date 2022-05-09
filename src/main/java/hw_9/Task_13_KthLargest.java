4e8e9e69028af11687a876bc48e5c8bcffa76d58
Aleksei Chapaev <alekseiachapaev@gmail.com>
2022-05-09 20:12:45 +0200

    add Task_13_KthLargest

---
+++ src/main/java/hw_9/Task_13_KthLargest.java
@@ -0,0 +1,23 @@
+package hw_9;
+
+public class Task_13_KthLargest {
+
+    public int KthLargest(int[] arr, int k){
+
+        if(arr != null && arr.length > 0 && k < arr.length){
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = i + 1; j < arr.length; j++) {
+                    if(arr[i] > arr[j]){
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            return arr[arr.length - k];
+        }
+
+        return 0;
+    }
+}
